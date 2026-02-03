20ad0629bfbf1e4d8cfca16d90253898afbb5acb
TrackerJo <nkemme54@gmail.com>
2026-02-02 21:58:19 -0500

    sync

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const server = require('express')();
 const http = require('http').createServer(server);
 const io = require('socket.io')(http, {
     cors: {
-        origin: "https://trackerjo.github.io",
-        // origin: "http://localhost:8080",
+        // origin: "https://trackerjo.github.io",
+        origin: "http://localhost:8080",
         methods: ["GET", "POST"],
         credentials: true
     }
